50d38576154473a0f05ef74b229a1bdaff19a684
Gamafoot <gamafootsas@gmail.com>
2023-12-18 17:10:14 +0700

    init urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 
 api_v1 = [
+    path('', include('apps.authentication.urls')),
     path('', include('apps.clinics.urls')),
     path('', include('apps.users.urls')),
-    path('', include('apps.surgeons.urls'))
+    path('', include('apps.surgeons.urls')),
+    path('', include('apps.services.urls')),
+    path('', include('apps.articles.urls'))
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path('api/v1/', include(api_v1))
+    # path("__debug__/", include("debug_toolbar.urls")),
+    path('api/v1/', include(api_v1)),
 ]
+
+urlpatterns += doc_urls
