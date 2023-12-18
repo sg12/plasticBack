61cd46c37b65504338b9165cc168233a9a1e64b8
Gamafoot <gamafootsas@gmail.com>
2023-12-18 19:33:18 +0700

    add logout

--- src/apps/authentication/urls.py
+++ src/apps/authentication/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('auth/login/', LoginView.as_view()),
     path('auth/register/<path:type>/', RegisterView.as_view()),
+    path('auth/logout/', LogoutView.as_view()),
 ]
---
+++ src/apps/authentication/views/logout.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        token = Token.objects.filter(user=request.user)
+
+        if token:
+            token.delete()
+        
+        return Response({'message': 'Вы успешно вышли из аккаунта'})
