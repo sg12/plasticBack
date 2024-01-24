dde18f0fc690bbf4b810938e098f15df25f4290e
Gamafoot <gamafootsas@gmail.com>
2024-01-25 06:25:15 +0700

    edit name of a serializer class

--- src/apps/users/views/user.py
+++ src/apps/users/views/user.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.clients.serializers import ClientRetrieveSerializer
 from apps.surgeons.serializers import SurgeonRetrieveSerializer
-from apps.clinics.serializers import ClinicReadSerializer
+from apps.clinics.serializers import ClinicRetrieveSerializer
 from apps.clients.models import Client
 from apps.surgeons.models import Surgeon
 from apps.clinics.models import Clinic
@@ -19,6 +19,6 @@ class UserMeView(APIView):
             case 'surgeon':
                 serializer = SurgeonRetrieveSerializer(Surgeon.objects.get(user=request.user))
             case 'clinic':
-                serializer = ClinicReadSerializer(Clinic.objects.get(user=request.user))
+                serializer = ClinicRetrieveSerializer(Clinic.objects.get(user=request.user))
 
         return Response(serializer.data)
