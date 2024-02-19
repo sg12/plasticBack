5a1da710eea870950f8c995d79ecea05bac078e8
Gamafoot <gamafootsas@gmail.com>
2024-02-19 14:26:48 +0700

    fix media view

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 api_v1 = [
@@ -24,3 +26,6 @@ urlpatterns = [
 urlpatterns += doc_urls
 
 handler404 = 'core.views.custom_handler_404'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
