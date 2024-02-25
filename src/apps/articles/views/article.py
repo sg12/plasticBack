8fa0ace6a0361af93e0f349e534cf1ed7ceb6c52
Gamafoot <gamafootsas@gmail.com>
2024-02-25 15:52:35 +0700

    add header X-Total-Count

--- src/apps/articles/views/article.py
+++ src/apps/articles/views/article.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from apps.articles.models import Article
 from apps.articles.serializers import ArticleReadSerializer
+from rest_framework.response import Response
 
 
 __all__ = (
@@ -8,9 +9,17 @@ __all__ = (
     'ArticleRetrieveView'
 )
 
+
 class ArticleListView(ListAPIView):
     queryset = Article.objects.all().order_by('-id')
     serializer_class = ArticleReadSerializer
+
+    def get(self, request):
+        queryset = self.get_queryset()
+        serializer = ArticleReadSerializer(queryset, many=True)
+        response = Response(data=serializer.data)
+        response.headers['X-Total-Count'] = len(serializer.data)
+        return response
 
 
 class ArticleRetrieveView(RetrieveAPIView):
