1380b50be0930abb7bdd1a37b480030761f8402a
Gamafoot <gamafootsas@gmail.com>
2024-02-26 08:23:48 +0700

    edit media route

--- src/core/urls.py
+++ src/core/urls.py
@@ -27,5 +27,5 @@ urlpatterns += doc_urls
 
 handler404 = 'core.views.custom_handler_404'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
