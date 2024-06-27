a3dedcd6497a8aeb03c2cbd2f13b46446eb85c4b
Gamafoot <gamafootsas@gmail.com>
2024-06-28 06:08:56 +0700

    edit permissions

--- src/apps/clinic/permissions/is_clinic.py
+++ src/apps/clinic/permissions/is_clinic.py
@@ -3,13 +3,5 @@ from apps.user.models import User, Role
 
 
 class IsClinic(permissions.BasePermission):
-    def has_permission(self, request, view):
-        user = None
-        user_pk = view.kwargs.get('user_pk')
-        
-        if user_pk:
-            user = User.objects.get(pk=user_pk)
-        else:
-            user = request.user
-        
-        return user.role.name == Role.CLINIC
+    def has_permission(self, request, view):        
+        return request.user.role.name == Role.CLINIC
--- src/pkg/permissions/is_doctor_or_clinic.py
+++ src/pkg/permissions/is_doctor_or_clinic.py
@@ -1,21 +1,13 @@
 from rest_framework import permissions
-from apps.user.models import User, Role
+from apps.user.models import Role
 
 
 class IsDoctorOrClinic(permissions.BasePermission):    
     def has_permission(self, request, view):
-        user = None
-        
-        user_pk = view.kwargs.get('user_pk')
-        if user_pk:
-            user = User.objects.get(pk=user_pk)
-        else:
-            user = request.user
-        
         if request.method == permissions.SAFE_METHODS:
             return True
         
-        is_doctor = user.name == Role.DOCTOR
-        is_clinic = user.type == Role.CLINIC
+        is_doctor = request.user.role.name == Role.DOCTOR
+        is_clinic = request.user.role.name == Role.CLINIC
         
         return is_doctor or is_clinic
