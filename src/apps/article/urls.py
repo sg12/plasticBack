14fcb2b8573a0d8359228a2b2c63b0f457a3b6b9
Gamafoot <gamafootsas@gmail.com>
2024-06-29 02:12:43 +0700

    edit url

--- src/apps/article/urls.py
+++ src/apps/article/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('articles/', ArticleListView.as_view()),
-    path('articles/<int:pk>/', ArticleRetrieveView.as_view()),
+    path('articles', ArticleListView.as_view()),
+    path('articles/<int:pk>', ArticleRetrieveView.as_view()),
 ]
