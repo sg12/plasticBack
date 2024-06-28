3e3f41157a5e1adf8b93ab23b94bb745759c80c2
Gamafoot <gamafootsas@gmail.com>
2024-06-29 05:42:34 +0700

    remove custom context in mixin

--- src/pkg/mixins/create.py
+++ src/pkg/mixins/create.py
@@ -5,13 +5,7 @@ class CreateModelMixin:
     result_many = False
     
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(
-            data=request.data,
-            context={
-                'request': self.request, 
-                'kwargs': self.kwargs
-            }
-        )
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         instance = serializer.save()
         serializer = self.result_class(instance=instance, many=self.result_many)
