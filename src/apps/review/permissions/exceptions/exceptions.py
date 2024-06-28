9b7412050fe20f44364ead7958e9eee65eeff664
Gamafoot <gamafootsas@gmail.com>
2024-06-29 02:15:43 +0700

    fix status code

--- src/apps/review/permissions/exceptions/exceptions.py
+++ src/apps/review/permissions/exceptions/exceptions.py
@@ -28,6 +28,6 @@ class IsNotAuthorReply(exceptions.APIException):
 
 
 class YouHaveReview(exceptions.APIException):
-    status_code = status.HTTP_403_FORBIDDEN
+    status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _('Вы не можете создать больше одного отзыва на этот аккаунт')
     default_code = 'permission_denied'
