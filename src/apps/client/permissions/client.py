24b9e5a3efed6aa38a9f110d89267714fe36dc98
Gamafoot <gamafootsas@gmail.com>
2024-06-29 02:13:39 +0700

    edit client permissions

--- src/apps/client/permissions/client.py
+++ src/apps/client/permissions/client.py
@@ -1,16 +1,22 @@
 from rest_framework import permissions
 from apps.user.models import Role
+from .excepitons import IsNotClient
 
 
 class IsClient(permissions.BasePermission):
     def has_permission(self, request, view):        
-        return request.user.role.name == Role.CLIENT
+        condition = request.user.role.name == Role.CLIENT
+        if not condition:
+            raise IsNotClient
+        return True
 
 
 class IsClientOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user.role.name == Role.CLIENT
-        )
-            
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        condition = request.user.role.name == Role.CLIENT
+        if not condition:
+            raise IsNotClient
+        return True
---
+++ src/apps/client/permissions/excepitons/__init__.py
@@ -0,0 +1,1 @@
+from .is_not_client import IsNotClient
