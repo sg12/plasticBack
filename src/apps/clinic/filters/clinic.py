b1a858ae75f3bf9939c047207eaec4d56d77c7f2
Gamafoot <gamafootsas@gmail.com>
2024-06-30 06:09:49 +0700

    edit clinic filter

--- src/apps/clinic/filters/clinic.py
+++ src/apps/clinic/filters/clinic.py
@@ -9,35 +9,14 @@ class NumberFilter(filters.NumberFilter):
         return qs
 
 
+# TODO: что за специализация
+
 class ClinicFilter(filters.FilterSet):
-    specialization = filters.CharFilter(field_name='')
+    # specialization = filters.CharFilter(field_name='')
     metro = filters.CharFilter(field_name='metro__slug')
     district = filters.CharFilter(field_name='district__slug')
     city = filters.CharFilter(field_name='city__slug')
-    reception = filters.CharFilter(method='get_reception')
     ordering = filters.CharFilter(method='get_ordering')
-    
-    def get_reception(self, queryset, name, value):
-        params = {}
-        add_private = lambda: params.update({'private_reception': True})
-        add_clinic = lambda: params.update({'clinic_reception': True})
-        
-        if ',' in value:
-            values = value.split(',')
-            if 'private' in values:
-                add_private()
-            if 'clinic' in values:
-                add_clinic()
-        else:
-            match value:
-                case 'private':
-                    add_private()
-                case 'clinic':
-                    add_clinic()
-        
-        queryset = queryset.filter(**params)
-
-        return queryset
 
     def get_ordering(self, queryset, name, value):
         params = []
