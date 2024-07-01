2e198afdfd4514719526acc535565d8655e176db
Gamafoot <gamafootsas@gmail.com>
2024-07-01 14:50:52 +0700

    fix serializer

--- src/apps/doctor/serializers/clinic.py
+++ src/apps/doctor/serializers/clinic.py
@@ -4,9 +4,9 @@ from apps.clinic.models import Clinic
 
 class ClinicInfoSerilaizer(serializers.ModelSerializer):
     name = serializers.CharField(source='user.username')
-    metro = serializers.CharField(source='metro.name')
-    district = serializers.CharField(source='district.name')
-    city = serializers.CharField(source='city.name')
+    metro = serializers.CharField(source='metro.name', default=None)
+    district = serializers.CharField(source='district.name', default=None)
+    city = serializers.CharField(source='city.name', default=None)
     
     class Meta:
         model = Clinic
