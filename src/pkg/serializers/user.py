b5a7b155617a653fe9a32f8c7143af7ad1906a8f
Gamafoot <gamafootsas@gmail.com>
2024-08-19 15:30:03 +0900

    add custom CurrentUserDefault

--- src/pkg/serializers/user.py
+++ src/pkg/serializers/user.py
@@ -1,3 +1,22 @@
+class CurrentUserDefault:
+    requires_context = True
+    user_field = None
+    
+    def __init__(self, user_field: str = None):
+        self.user_field = user_field
+
+    def __call__(self, serializer_field):
+        user = serializer_field.context['request'].user
+        
+        if self.user_field:
+            return getattr(user, self.user_field)
+        
+        return user
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
+
+
 class UserUpdate:
     def update(self, instance, validated_data):
         exist = validated_data.get('user')
